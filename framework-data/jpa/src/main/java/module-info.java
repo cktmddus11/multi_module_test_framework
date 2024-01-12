4dc7246d6b1419e085bb3a65607546a33c3f5943
sy.cha <sy.cha@quintet.co.kr>
2024-01-12 17:35:59 +0900

    framework 모듈의 하위 의존성 가져오도록 수정

--- framework-data/jpa/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module maestro.quintet.crm.jpa {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens maestro.quintet.crm.jpa to javafx.fxml;
-    exports maestro.quintet.crm.jpa;
-}
