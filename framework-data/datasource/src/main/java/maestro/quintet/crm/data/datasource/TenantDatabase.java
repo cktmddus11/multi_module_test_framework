70008c4f16713a5900a3cb9661db30b8ae8cdd4e
sy.cha <sy.cha@quintet.co.kr>
2024-01-25 16:48:56 +0900

    enum 값 스키마명저장하도록 수정

--- framework-data/datasource/src/main/java/maestro/quintet/crm/data/datasource/TenantDatabase.java
+++ framework-data/datasource/src/main/java/maestro/quintet/crm/data/datasource/TenantDatabase.java
@@ -2,7 +2,6 @@ package maestro.quintet.crm.data.datasource;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
 
@@ -13,14 +12,15 @@ import java.util.Arrays;
 @Getter
 @AllArgsConstructor
 public enum TenantDatabase {
-    TENANTA("TENANT_A"),
-    TENANTB("TENANT_B")
+    //  요청시 헤더에서 넘길 테넌트 코드(스키마에 사용하는 테넌트명 으로 코드 등록)
+    TENANTA("tenantA"),
+    TENANTB("tenantB")
     ;
 
-    private String tenantCode;
+    private String tenantSchemaNm;
 
-    public static TenantDatabase convertEumCode(String value){
-        return Arrays.stream(TenantDatabase.values()).filter(c -> c.tenantCode.equals(value))
+    public static TenantDatabase convertEumCode(String tenantCode){
+        return Arrays.stream(TenantDatabase.values()).filter(c -> c.tenantSchemaNm.equals(tenantCode))
                 .findFirst().orElseThrow(() -> new RuntimeException("tenant code mapping error"));
     }
 }
--- framework-data/datasource/src/main/java/maestro/quintet/crm/data/datasource/TenantRoutingDataSourceContextHolder.java
+++ framework-data/datasource/src/main/java/maestro/quintet/crm/data/datasource/TenantRoutingDataSourceContextHolder.java
@@ -7,6 +7,7 @@ import org.springframework.util.Assert;
  * @since 1.0
  */
 public class TenantRoutingDataSourceContextHolder { //  TODO  필터에서 SCOPE 에 저장하니까 필요없,
+    // TODO 스키마가 변경됐는데 PK 값이 동일한게 있으면 영속성 컨텍스트는 이를 알지 못하기 때문에 1차캐시에서 데이터를 전달함. 이를 막기위해 해당 클래스를 빈처리 하고 EntityManager을 사전에 초기화 해주어야한다.
     private static ThreadLocal<TenantDatabase> CONTEXT = new ThreadLocal<>();
     public static void set(TenantDatabase tenantDatabase) {
         Assert.notNull(tenantDatabase, "clientDatabase cannot be null");
